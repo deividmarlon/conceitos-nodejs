74efa6f14ddbd3066b0412f958308d8529534ea0
Deivid Márlon <deividmarlon@gmail.com>
2020-08-18 12:26:18 -0300

    Created CRUD and LIKES

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,99 @@ app.use(cors());
 
 const repositories = [];
 
+function logRequests(request,response,next) {
+    const {method, url} = request;
+
+    const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+    console.time(logLabel);
+
+    next();
+
+    console.timeEnd(logLabel);
+
+}
+
+app.use(logRequests);
+
+function validateRepository(request,response,next){
+    const { id } = request.params;
+
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+    if(repositoryIndex >=0 ){
+      return next();
+    }
+
+    return response.status(400).send();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+    
+    return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs, likes } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository);
+
+  return response.status(200).json(repository);
+
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateRepository, (request, response) => {
+    const { id } = request.params;
+    const { title, url, techs } = request.body;
+
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+    if(repositoryIndex>=0){
+      repositories[repositoryIndex].title = title;
+      repositories[repositoryIndex].url = url;
+      repositories[repositoryIndex].techs = techs;
+      
+      return response.status(200).json(repositories[repositoryIndex]);
+    }
+
+    return response.status(400).json({message: 'Error to update!'});
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateRepository, (request, response) => {
+    const { id } = request.params;
+
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+    
+    if(repositoryIndex>=0){
+      repositories.splice(repositoryIndex, 1);
+      return response.status(204).send();
+    }
+
+    else{
+      return response.status(400).json({error: 'Project not Found!'});
+    }
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like",validateRepository, (request, response) => {
+    const { id } = request.params;
+
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+    if(repositoryIndex>=0){
+      repositories[repositoryIndex].likes += 1;
+     
+      return response.status(200).json(repositories[repositoryIndex]);
+    }
+
+    return response.status(400).json({message: 'Error to update!'});
 });
 
 module.exports = app;
